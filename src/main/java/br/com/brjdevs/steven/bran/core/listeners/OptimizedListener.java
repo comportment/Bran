fcc0bfb0366c71d24b5c54fd20d11e046f5a55f5
StupPlayer <stupplayer@outlook.com>
2017-02-18 22:43:22 -0200

    Oops, forgot to commit this class.

---
+++ src/main/java/br/com/brjdevs/steven/bran/core/listeners/OptimizedListener.java
@@ -0,0 +1,21 @@
+package br.com.brjdevs.steven.bran.core.listeners;
+
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.hooks.EventListener;
+
+public abstract class OptimizedListener<T extends Event> implements EventListener {
+	
+	private final Class<T> tClass;
+	
+	protected OptimizedListener(Class<T> tClass) {
+		this.tClass = tClass;
+	}
+	
+	public abstract void event(T event);
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public void onEvent(Event event) {
+		if (tClass.isInstance(event)) event((T) event);
+	}
+}
